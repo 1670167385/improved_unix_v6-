5f4b4d389d5967ee162128e8c2360f2647bcbf3b
1670167385 <1670167385@qq.com>
2023-01-11 01:40:10 +0800

    copy_changes

--- src/program/helloworld.c
+++ src/program/helloworld.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-#include<TimeInterrupt.h>
+//#include<TimeInterrupt.h>
 
 void main1(int argc, char **argv)
 {
     //unsigned int ads = GetTable();
     printf("hello unixv6++!\n");
-    Time::time = -1;
+    //Time::time = -1;
     exit(0);    
 }
